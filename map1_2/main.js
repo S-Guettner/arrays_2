71de94fd603a167fa3ba882934f82b53020cc14c
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-02-01 13:32:16 +0100

    finish

---
+++ map1_2/main.js
@@ -0,0 +1,20 @@
+let getraenke = [
+    "Coca-Cola",
+    "Apfelsaft",
+    "Pepsi",
+    "Traubensaft",
+    "Sprite",
+    "Orangensaft",
+    "Red Bull Energy Drink",
+    "Fanta"
+    ]
+
+    const upperDrinks = getraenke.map((element) => {
+        return element.toUpperCase()
+    })
+    console.log(upperDrinks)
+
+    getraenke.map((element) => {
+        return element.toUpperCase()
+    })
+    console.log(getraenke)
