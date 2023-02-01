d431624fbde90d91674e1f5823b4798576f9b897
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-02-01 13:14:32 +0100

    finish

---
+++ forEach1_1/main.js
@@ -0,0 +1,17 @@
+let getraenke = [
+    "Coca-Cola",
+    "Apfelsaft",
+    "Pepsi",
+    "Traubensaft",
+    "Sprite",
+    "Orangensaft",
+    "Red Bull Energy Drink",
+    "Fanta"
+    ]
+
+getraenke.sort()
+
+    getraenke.forEach((element) => {
+        console.log(element)
+        document.write(element,"<br>")
+    })
